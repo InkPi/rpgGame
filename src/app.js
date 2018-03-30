9bfa7f23a60f234d5c6e8a5f1eee15edb5da5271
Social Impact (6) <lz28330@gmail.com>
2018-03-30 15:43:44 -0400

    trouble with looping

--- src/app.js
+++ src/app.js
@@ -39,8 +39,8 @@ $(document).ready(function() {
     playerSide.attr('class', 'playerSide');
     let playerAttk = $('#playerAttk');
     //https://api.jquery.com/data/
-    $(playerSide).data('hp', 620);
-    $(playerAttk).data('attk', 43);
+    playerSide.data('hp', 620);
+    playerAttk.data('attk', 43);
 
    $('.playerDiv').append(playerSide);
 
@@ -58,8 +58,9 @@ $(document).ready(function() {
 
   //Get p.monHp for enemy and give data hp, attk
   var enemyP = $('.monHp');
-  $(enemyP).data('hp', 100).text(100); //can replace 100 w/ random# var
-  $(enemyP).data('attk', 20);
+  //http://api.jquery.com/text/
+  enemyP.data('hp', 100).text(100); //can replace 100 w/ random# var
+  enemyP.data('attk', 20);
 
 
 
@@ -67,21 +68,27 @@ $(document).ready(function() {
 
 
   /* Player Attacks Enemy!! */
-  $('#playerAttk').on('click', function(){
+  //for loop?
+  var pattack = playerAttk.on('click', function(){
     var $this = $(this);
+    //playerParagraph.hide().text();
     /* math here */
     //keep decrement enemy hp by player attk
 
     // for (let i=43; i => 0; i++) {
 
     // get the enemy's hp
-    const enemyHP = $(enemyP).data('hp');
+    const enemyHP = enemyP.data('hp');
     //get player's attk
-    const playerDmg = $(playerAttk).data('attk');
+    const playerDmg = playerAttk.data('attk');
     // do the math and minus hp with player attk
     var enemyHpDmg = enemyHP - playerDmg;// $() made it an obj!
-    //text enemy left over pts and dialog
-    $(enemyP).data('hp', enemyHpDmg).text(enemyHpDmg); //had help with this
+    //text enemy left over hp pts and dialog
+    enemyP.data('hp', enemyHpDmg).text(enemyHpDmg); //had help with this
+
+    let enemyPara = $('.monPara');
+    enemyPara.text('You attacked that cute monster! 43 attk pts');
+
     // enemyP.text(`${enemyHpDmg}`);
 
     //css movement from attack
@@ -91,33 +98,29 @@ $(document).ready(function() {
 
     //did var pdamage
     //Text when attack monster
-    $(enemyHpDmg).each(function() {
-        var enemyPara = $('.monPara');
-        $(enemyPara).text('You attacked that cute monster! 43 attk pts');
-    })
+    //console.log(enemyHpDmg);
+
+
+    //activate enemy turn after attacking
 
     //if damage=0 alert "won"
     //then reload the whole page
     if (enemyHpDmg <= 0) {
-      window.alert('you won!'); //had to add window! Solved it!
-      window.location.reload(true); //did this before but got it from: https://developer.mozilla.org/en-US/docs/Web/API/Location/reload
+      //fade out enemy img
+
+      //https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_eff_fadeout_fadein
+      //http://api.jquery.com/animate/
+      window.alert('you are victorious and was able to overcome the creature\'s cuteness!'); //had to add window! Solved it!
+      // window.location.reload(true); //did this before but got it from: https://developer.mozilla.org/en-US/docs/Web/API/Location/reload
     }
 
     //computer generate enemy attack now! return enemy attk func here
 
-    // var $this = $(this);
-    // if(this.data('clicked')) {
-    //   console.log('enemy attk');
-    // }
-    if($this.data('clicked')) {
-      $(enemyHpDmg).each(function() {
-      console.log("atkk");
-    })
-    }
-  });//player attk onclick
+    //after player attack, monster attack now, then player etc
 
 
-  /*Now Enemy Attacks Player!*/
+
+  //player attk onclick
 
   //random generated hp and attk
 
@@ -128,9 +131,45 @@ $(document).ready(function() {
   //hp txt and dialog from attack
 
 
+/*Now Enemy Attacks Player!*/
+function enemyTurn() {
+        //hide the enemy's dialogue text
+        enemyPara.hide().text(); //http://api.jquery.com/animate/
+        //player's hp
+        let playerHp = playerSide.data('hp');
+        //enemy attk pt
+        let enemyAttk = enemyP.data('attk');
+        //math
+        //playerHp = playerHp - enemyAttk;
+
+        const playerHpDmg = playerHp - enemyAttk;
+        let hpScore = $('#hp');
+        console.log(hpScore);
+        //text
+        console.log(playerHpDmg);
+        //hpScore.text(playerHpDmg);
+        hpScore.data('hp', playerHpDmg).text(playerHpDmg);
+        playerSide.data('hp', playerHpDmg).text(playerHpDmg);
+        //animate css (left, right) on player Img
+
+        //get .playerPara
+        var playerParagraph = $('.playerPara');
+        playerParagraph.text("'Arg! I got attacked by this horrible enemy!'");
+
+
+    }
+    //https://www.sitepoint.com/jquery-settimeout-function-examples/
+  setTimeout(enemyTurn, 2000);
+
+
+
+  // function redo() {
+  //   playerParagraph.hide().text();
+  // }
+
+  // setTimeout(redo, 3000);
 
   });//button clicked
-
 
 
 
@@ -142,4 +181,5 @@ $(document).ready(function() {
   //exploration link helps:
   /* http://jsfiddle.net/jakecigar/u2dtep5n/
   http://api.jquery.com/animate/ */
+});
 });
